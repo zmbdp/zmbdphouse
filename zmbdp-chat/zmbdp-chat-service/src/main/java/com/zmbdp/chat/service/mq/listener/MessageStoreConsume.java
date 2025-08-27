01f604ebc830fe00480dc0253b69d34496323b48
zmbdp <3566983494@qq.com>
2025-08-28 01:04:20 +0800

    稚能安居平台

--- zmbdp-chat/zmbdp-chat-service/src/main/java/com/zmbdp/chat/service/mq/listener/MessageStoreConsume.java
+++ zmbdp-chat/zmbdp-chat-service/src/main/java/com/zmbdp/chat/service/mq/listener/MessageStoreConsume.java
@@ -13,7 +13,6 @@ import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
 /**
  * 持久化聊天消息
  *
